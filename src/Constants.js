bd6730c97a1350cace23126ee3dc9104630ddeae
aliceresende <97093442+aliceresende@users.noreply.github.com>
2025-02-14 00:46:20 +0000

    final project

--- src/Constants.js
+++ src/Constants.js
@@ -1,84 +1,32 @@
 
 
-import DROPSHIP from "./Assets/Projects/dropship.jpg";
-import GOOD_FOOD_DISCOUNTS from "./Assets/Projects/goodFoodDiscounts.png";
-import GUAMEDIA from "./Assets/Projects/guamedia.jpg";
-import HSP from "./Assets/Projects/hsp.png";
-import COSMOS from "./Assets/Projects/cosmos.jpg";
-import TECHSPEC from "./Assets/Projects/techspec.png";
-import TRAVELWELL from "./Assets/Projects/travelwell.png";
+import ECOURSE from "./Assets/Projects/ECourse.webp";
+import FARM from "./Assets/Projects/FarmApp.webp";
+import ROBOTGO from "./Assets/Projects/RobotGO.webp";
 
 
 export const PROJECTS = [
   {
-    image: COSMOS,
-    name: "Micorsoft Azure Cosmos DB",
-    technologyUsed: "ReactJs, Javascript, Redux, knockout.js, Jest",
-    description: "aern app development. Single-digit millisecond response times, and automatic and instant scalability, guarantee speed at any scale.",
-    url: "https://cosmos.azure.com/",
+    image: FARM,
+    name: "Farm Management Application",
+    technologyUsed: "Java, C, Assembly",
+    description: "",
+    url: "https://github.com/aliceresende/ISEP-LAPR3-2022-2023",
   },
   {
-    image: TRAVELWELL,
-    name: "travlwell.com",
-    technologyUsed: "Shopify, React, typescript, polaris, GraphQL",
-    description: "Custom Luggage Tags and Passport Holders engraved with everywhere you've traveled and everywhere you will travel in the future.",
-    url: "https://travlwell.com/",
+    image: ECOURSE,
+    name: "eCourse Platform",
+    technologyUsed: "Java, ANTLR, Spring Boot",
+    description: "",
+    url: "https://github.com/aliceresende/ISEP-LAPR4-2022-2023",
   },
 
   {
-    image: GUAMEDIA,
-    name: "GuaMedia",
-    technologyUsed: "ReactNative, Javascript, Redux",
-    description: "orking platform where users can build their own user profiles, create their own posts, Livestream broadcast and follow each other",
-    url: "https://play.google.com/store/apps/details?id=com.tiu.guo.media",
-  },
-  {
-    image: TECHSPEC,
-    name: "TechSpec",
-    technologyUsed: "ReactJs, Typescript, Redux",
-    description: "TechSpec is e-commerce web app. User can compare his product and purchange. This had a dashboard web app which had the data of orders placed and type of user.",
-    url: "",
-  },
-  {
-    image: HSP,
-    name: "HSP (Health Solution Plus)",
-    technologyUsed: "ReactJs, Javascript, Redux",
-    description: " addition/updation of codes and categories.",
-    url: "",
-  },
-  {
-    image: DROPSHIP,
-    name: "DropShip Shopify App",
-    technologyUsed: "Shopify, React, typescript, polaris, GraphQL, Nodejs, Koajs",
-    description: "detail to store owner. Also maintain tracking etc of product.",
-    url: "",
-  },
-  {
-    image: GOOD_FOOD_DISCOUNTS,
-    name: "GoodFoodDiscounts",
-    technologyUsed: "ReactNative, Javascript, Redux",
-    description: ". Even has facility to add coupan. You can add food in favourite list.",
-    url: "",
-  },
+    image: ROBOTGO,
+    name: "RobDroneGo Application",
+    technologyUsed: "Angular, HTML, SCSS, JS",
+    description: "",
+    url: "https://github.com/aliceresende/ISEP-LAPR5-2023-2024",
+  }
 ];
 
-export const SKILLS = [
-  { name: "ReactJs", initialRating: 4 },
-  { name: "React-Native", initialRating: 4 },
-  { name: "JavaScript", initialRating: 4 },
-  { name: "Redux", initialRating: 4 },
-  { name: "Shopify/Reactjs", initialRating: 4 },
-  { name: "Nodejs", initialRating: 3 },
-  { name: "Koajs", initialRating: 3 },
-  { name: "Html", initialRating: 4 },
-  { name: "CSS", initialRating: 4 },
-  { name: "Unit-Testing(Jest)", initialRating: 3 },
-  { name: "TypeScript", initialRating: 4 },
-  { name: "Graphql", initialRating: 4 },
-  { name: "Apollo", initialRating: 4 },
-  { name: "AntD | ReactStrap | Material UI", initialRating: 4 },
-  { name: "Shopify Polaris", initialRating: 4 },
-  { name: "Git", initialRating: 4 },
-];
-
-export const TOOLS = ["Visual Studio Code", "Git", "Chrome DevTools", "Npm (Node Package Manager)", "Atom", "Sublime Text", "Linux", "Heroku", "Postman"]
